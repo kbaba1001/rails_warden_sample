4b57cd69aab4eee0702f433083bd455fa05c4f64
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-11-02 15:47:19 +0900

    remove warden default_strategy
    strategy なくても動くかも。
    
    `request.env['warden'].authenticate` を使うときは strategy が必要だが、
    `request.env['warden'].set_user` を使えば認証したあとの状態を作れる

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,8 +1,6 @@
 require Rails.root.join('lib/auth/failure_app')
-require Rails.root.join('lib/auth/password_strategy')
 
 Rails.configuration.middleware.use RailsWarden::Manager do |manager|
-  # manager.default_strategies :password_strategy
   manager.failure_app = Auth::FailureApp
 end
 
