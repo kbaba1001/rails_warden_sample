78f1d5461fecddccc63482ccbf1ae8eb0d33cbfe
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-11-02 18:09:37 +0900

    remove rails_warden setting

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -1,8 +1,14 @@
 class Users::ApplicationController < ApplicationController
+  helper_method :current_user
+
+  def current_user
+    warden.user(:user)
+  end
+
   private
 
   def authenticate_user!
-    unless authenticated?(:user)
+    unless warden.authenticated?(:user)
       redirect_to new_session_path
     end
   end
