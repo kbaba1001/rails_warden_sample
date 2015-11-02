5a14f9d0cf01561d94ec8d51cde793015c104195
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-11-02 15:58:14 +0900

    dashboard require authenticate user

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -1,2 +1,9 @@
 class Users::ApplicationController < ApplicationController
+  private
+
+  def authenticate_user!
+    unless authenticated?
+      redirect_to new_session_path
+    end
+  end
 end
--- app/controllers/users/dashboards_controller.rb
+++ app/controllers/users/dashboards_controller.rb
@@ -1,4 +1,6 @@
 class Users::DashboardsController < Users::ApplicationController
+  before_action :authenticate_user!
+
   def show
   end
 end
